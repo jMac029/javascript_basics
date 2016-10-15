4cfcbb046049bc0eb07d5765fbe1cc09c7386e40
James McMillan <jmcmillan@hri.uci.edu>
2016-10-14 17:04:35 -0700

    javascript variables course complete
    notes and challenges from javascript variables course on treehouse

---
+++ introducing_variables/stage_4_working_with_strings_and_finding_help/scripts.js
@@ -0,0 +1,6 @@
+var stringToShout = prompt("What should I shout?");
+var shout = stringToShout.toUpperCase();
+shout += "!!!";
+alert(shout);
+
+                      
---
+++ introducing_variables/stage_5_the_variable_challenge/story.js
@@ -0,0 +1,8 @@
+var noun = prompt("Please type in a noun.");
+var verb = prompt("Please type in a verb.");
+var adjective = prompt("Please type in an adjective.");
+var sentence = "The " + noun;
+sentence += " " + verb;  
+sentence += " over the";
+sentence += " " + adjective + " moon.";
+document.write(sentence);
